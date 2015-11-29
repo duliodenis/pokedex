4eba33fc3bea4015f07604200c62017a59216931
Dulio Denis <dulio.denis@yahoo.com>
2015-11-29 09:49:50 -0500

    Added UISearchBar delegates for search filtering

--- Pokedex/Pokedex/ViewController.swift
+++ Pokedex/Pokedex/ViewController.swift
@@ -9,12 +9,15 @@
 import UIKit
 import AVFoundation
 
-class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UISearchBarDelegate {
 
     @IBOutlet weak var collection: UICollectionView!
+    @IBOutlet weak var searchBar: UISearchBar!
     
     var pokemon = [Pokemon]()
+    var filteredPokemon = [Pokemon]()
     var music: AVAudioPlayer!
+    var inSearchMode = false
 
     
     // MARK: View Lifecycle
@@ -24,11 +27,16 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         collection.delegate = self
         collection.dataSource = self
         
+        searchBar.delegate = self
+        searchBar.returnKeyType = UIReturnKeyType.Done
+        
         initAudio()
         music.play()
         parsePokemonCSV()
     }
 
+    
+    // MARK: Utility Functions
     
     func parsePokemonCSV() {
         let path = NSBundle.mainBundle().pathForResource("pokemon", ofType: "csv")!
@@ -71,15 +79,22 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
             sender.alpha = 1.0
             sender.setImage(UIImage(named: "volume-on"), forState: .Normal)
         }
-        
     }
     
     
-    // MARK: UICollectionView Delegates
+    // MARK: Collection View Delegates
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         if let cell = collectionView.dequeueReusableCellWithReuseIdentifier("PokeCell", forIndexPath: indexPath) as? PokeCell {
-            let poke = pokemon[indexPath.row]
+            
+            let poke: Pokemon!
+            
+            if inSearchMode {
+                poke = filteredPokemon[indexPath.row]
+            } else {
+                poke = pokemon[indexPath.row]
+            }
+            
             cell.configureCell(poke)
             return cell
         } else {
@@ -94,7 +109,11 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     
     
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 718
+        if inSearchMode {
+            return filteredPokemon.count
+        } else {
+            return pokemon.count
+        }
     }
     
     
@@ -106,5 +125,26 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
         return CGSizeMake(105, 105)
     }
+    
+    
+    // MARK: Search Bar Delegates
+    
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        view.endEditing(true)
+    }
+ 
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text == nil || searchBar.text == "" {
+            inSearchMode = false
+            view.endEditing(true)
+            collection.reloadData()
+        } else {
+            inSearchMode = true
+            // let lower = searchText!.lowercaseString
+            filteredPokemon = pokemon.filter({$0.name.rangeOfString(searchText, options: .CaseInsensitiveSearch) != nil})
+            collection.reloadData()
+        }
+    }
+    
 }
 
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Pokédex
 An iOS 9 Swift 2 Pokedex that uses the Pokémon RESTful API. All the Pokémon data you'll ever need right in your pocket.
 
-![](https://github.com/duliodenis/pokedex/blob/master/art/screenshot/pokedex2.gif)
+![](https://github.com/duliodenis/pokedex/blob/master/art/screenshot/pokedex3.gif)
 
 ### Licensing
 Pokédex is licensed under [the MIT License](https://github.com/duliodenis/pokedex/blob/master/LICENSE).
