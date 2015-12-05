3f10833d9bdebcb5c32fa2ca12b6b49f8664490c
Dulio Denis <dulio.denis@yahoo.com>
2015-12-05 14:56:46 -0500

    Added the outlets to the Detail VC and private properties to the Model Object

--- Pokedex/Pokedex/PokemonDetailVC.swift
+++ Pokedex/Pokedex/PokemonDetailVC.swift
@@ -11,11 +11,29 @@ import UIKit
 class PokemonDetailVC: UIViewController {
     
     @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var mainImage: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var defenseLabel: UILabel!
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var IDLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var attackLabel: UILabel!
+    
+    @IBOutlet weak var currentEvolutionImage: UIImageView!
+    @IBOutlet weak var nextEvolutionImage: UIImageView!
+    
+    @IBOutlet weak var evolutionLabel: UILabel!
     
     var pokemon: Pokemon!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         nameLabel.text = pokemon.name
+    }
+    
+    @IBAction func backAction(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
     }
 }
